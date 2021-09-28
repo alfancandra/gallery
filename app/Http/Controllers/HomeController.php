9d70e999d505db7cac947ecbdfdd429d739ca235
Alfan Adi Chandra <46060522+alfancandra@users.noreply.github.com>
2021-09-28 11:43:27 +0700

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,8 +11,9 @@ class HomeController extends Controller
     public function index(Request $request)
     {
         $photo = Photo::join('categories','categories.id','=','photos.category_id')
-        ->select('photos.id as idphoto','photos.title','photos.deskripsi','photos.images','categories.category')
+        ->select('photos.id as idphoto','photos.title','photos.deskripsi','photos.images','categories.category','categories.active')
         ->where('photos.active',1)
+        ->where('categories.active',1)
         ->orderBy('photos.created_at','DESC')->paginate(6);
         $category = Category::where('active',1)->get();
         
